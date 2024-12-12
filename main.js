c43652516aa38e77b911811f1edc2154d06b5b2d
Cody Bontecou <bontecouc@gmail.com>
2024-12-12 16:56:44 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,37 @@
+const { wrap, configure } = require('agentql')
+const { chromium } = require('playwright')
+
+const queryMap = async () => {
+    // Configure the AgentQL API key
+    configure({
+        apiKey: '',
+    })
+
+    // Launch a headless browser using Playwright.
+    const browser = await chromium.launch({ headless: false })
+
+    // Create a new page in the browser and wrap it to get access to the AgentQL's querying API
+    const page = await wrap(await browser.newPage())
+    await page.goto('https://www.google.com/maps')
+
+    const elements = await page.queryElements(`{ search_box }`)
+    await elements.search_box.type("'plumbing' inurl:business.site santa rosa")
+    await elements.search_box.press('Enter')
+
+    const data = await page.queryData(`{
+      business[] {
+        name
+        website_url
+        address
+        rating
+        review_count
+        service_type
+      }
+    }`)
+
+    console.log(data)
+}
+
+;(async () => {
+    await queryMap()
+})()
